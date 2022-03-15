2f972415652a4a73dc58eda9cb16fbac8deb4859
peter <peter@mancloud.eu>
2022-03-15 18:11:39 +0100

    ...

--- src/Services/SwaggerGeneratorService.php
+++ src/Services/SwaggerGeneratorService.php
@@ -462,7 +462,10 @@ class SwaggerGeneratorService
     {
         $properties = [];
         foreach ($parameters as $parameter_name => $parameter_info) {
-            $this->addProperty($parameter_name, $is_resource ? "string" : $parameter_info , $properties);
+            if(! is_numeric($parameter_name))
+            {
+                $this->addProperty($parameter_name, $is_resource ? "string" : $parameter_info , $properties);
+            }
         }
 
         return $properties;
@@ -507,6 +510,11 @@ class SwaggerGeneratorService
         } else {
             $property = [
                 'type' => 'array',
+                'items' => [
+                    'type' => 'object',
+                    'required' => $this->getRequiredParameters($property_rule),
+                    'properties' => $this->getProperties($property_rule),
+                ]
             ];
         }
 
