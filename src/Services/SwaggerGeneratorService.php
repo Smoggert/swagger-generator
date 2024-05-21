9fe657de9465442ce3267948efd35412a5a89748
peter <peter@mancloud.eu>
2024-05-21 19:14:38 +0200

    Sort by description is not effective.

--- src/Services/SwaggerGeneratorService.php
+++ src/Services/SwaggerGeneratorService.php
@@ -243,7 +243,7 @@ class SwaggerGeneratorService
             $this->addPath($paths, $route['route'], $route['tags']);
         }
 
-        usort($paths, fn($a, $b) => strcasecmp($a['summary'], $b['summary']));
+        ksort($paths);
 
         $swagger_docs['paths'] = $paths;
     }
